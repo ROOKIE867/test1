48b4d63349c4f67850d11e8ad01427c47ea43e64
sam <libaiwangxizhi@gmail.com>
2022-12-05 20:02:37 -0500

    thank you

--- src/com/qa/fifa/Main.java
+++ src/com/qa/fifa/Main.java
@@ -3,8 +3,8 @@ package com.qa.fifa;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("I added a new class in this project");
-
+		System.out.println("I added a new class in this project, thank you");
+		
 	}
 
 }
